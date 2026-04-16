cff97d1a66cb5ca2d3983a593f1ce154a49c8a66
WildhanValerian25 <wildanvalerian89@gmail.com>
2026-04-16 16:11:01 +0700

    code ini menampilkan database yang dikoneksikan dan  Inisialisasi Form Login

--- SistemPenjualanDiecastNew/Form1.cs
+++ SistemPenjualanDiecastNew/Form1.cs
@@ -3,4 +3,25 @@ using System.Drawing;   // Menambah librarty dasar dan namespace sesuai judul
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
-namespace SistemPenjualanDiecastNew // Menambahkan namespace sesuai judul
+namespace SistemPenjualanDiecastNew 
+{
+    public partial class Form1 : Form
+    {
+        // Deklarasi Komponen UI
+        Label lblTitle, lblUsername, lblPassword;
+        TextBox txtUsername, txtPassword;
+        Button btnLogin;
+        LinkLabel linkRegister;
+
+        // Baris 15: Variabel Koneksi Global (Agar tidak ada warning CS0414)
+        string connStr = @"Data Source=LAPTOP-24A5CGHI\WILDHANFIGHT;Initial Catalog=DiecastDB;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Menghubungkan LinkLabel ke method-nya
+            linkRegister.LinkClicked += LinkRegister_LinkClicked;
+        }
+
+       
