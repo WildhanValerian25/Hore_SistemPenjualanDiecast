ae372d6ffacd1a8ce2f05872aab25685d7a3ce87
WildhanValerian25 <wildanvalerian89@gmail.com>
2026-04-16 16:15:17 +0700

    Menghubungkan linklabel register dengan event handler

--- SistemPenjualanDiecastNew/Form1.cs
+++ SistemPenjualanDiecastNew/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Drawing;   // Menambah librarty dasar dan namespace sesuai judul
-using System.Windows.Forms;
+﻿using Microsoft.Win32;
+using System;
 using System.Data.SqlClient;
+using System.Drawing;   // Menambah librarty dasar dan namespace sesuai judul
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace SistemPenjualanDiecastNew 
 {
@@ -15,5 +17,14 @@ namespace SistemPenjualanDiecastNew
 
         // Baris 15: Variabel Koneksi Global (Agar tidak ada warning CS0414)
         string connStr = @"Data Source=LAPTOP-24A5CGHI\WILDHANFIGHT;Initial Catalog=DiecastDB;Integrated Security=True"; // Menambahkan koneksi string database
+
+        public Form1() 
+        {
+            InitializeComponent();
+
+        // menghubungkan LinkLabel register dengan event handler
+
+        linkRegister.LinkClicked += LinkRegister_LinkClicked;
+        } 
 
        
